a65e2f8cac9ecfa20a81c8790eb4b49607365f1d
Jonathan  Foster <jonathanfoster@Jonathans-MacBook-Pro.local>
2020-01-06 21:01:11 +0000

    Only snips on the 3rd go

--- basic-extension/popup.js
+++ basic-extension/popup.js
@@ -1,20 +1,32 @@
 console.log("background running");
 document.getElementById('snip').addEventListener('click', onSnip, false)
 document.getElementById('csv').addEventListener('click', csv, false)
+var dataRow = new Object();
+var fields = ["companyName", "firstName", "surname"];
+var i = 0;
+var fieldCount = fields.length;
 
 function onSnip() {
   chrome.tabs.getSelected(null, function(tab){
     
     chrome.tabs.sendRequest(tab.id, {method: "getSelection"}, function(response){
-      console.log(response.data);
-      $.post("http://127.0.0.1:5000/snip",
+      // send the data once the object has been completely filled
+      if (i == fieldCount) {
+        i = 0;
+        $.post("http://127.0.0.1:5000/snip",
         {
           tab: "Donald Duck",
           test: response.data
         },
         function(data, status){
         alert("Snip");
-    });
+        });
+      }
+      else {
+        i++;
+      }
+      
+  
     });
 });
 }
