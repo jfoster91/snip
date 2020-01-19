ec98e4332e2397a5aaf353f322abc7f410e46a9d
Jonathan  Foster <jonathanfoster@Jonathans-MacBook-Pro.local>
2020-01-19 19:17:17 +0000

    Working with new csv files being generated

--- application.py
+++ application.py
@@ -1,23 +1,36 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, send_from_directory
 import os.path 
 from os import path
 import csv
 import json
+import wget
+import uuid
+
 
 app = Flask(__name__)
 
-with open('test.csv', mode='w') as test_file:
-            test_writer = csv.writer(test_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-
-            test_writer.writerow(["Company Name", "First Name", "Surname"])
+fileName = "new.csv"
 
 @app.route("/")
 def index():
     
-    return "hello, world:!"
+    return "Snip running"
+
+@app.route("/start")
+def start():
+
+    global fileName 
+    fileName = str(uuid.uuid4()) + ".csv"
+
+    with open(fileName, mode='w') as test_file:
+            test_writer = csv.writer(test_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+            test_writer.writerow(["Company Name", "First Name", "Surname"])
+
+    return redirect("/")
 
 @app.route("/snip", methods=["GET", "POST"])
-def external():
+def snip():
     if request.method == "POST":
         print("Message received cowboy")
 
@@ -29,13 +42,22 @@ def external():
         for item in lst:
             print (item)
 
-        with open('test.csv', mode='a') as test_file:
+        with open(fileName, mode='a') as test_file:
             test_writer = csv.writer(test_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
             test_writer.writerow([lst[0], lst[1], lst[2]])
 
-            
+        test_file.close()
 
     return redirect("/")
 
+@app.route('/download', methods=["GET"]) 
+def download():
+    print("download request")
+
+    return send_from_directory('/Users/jonathanfoster/Google Drive/Life/Learning/Other Code Projects/servers/flask-server', filename=fileName, 
+        as_attachment=True, cache_timeout=-1)
+
+
+# Issue -> the test file keeps getting downloaded even though i am generating new files
 # Proposed solution... populate array in front end and then send that array to the backend
--- basic-extension/popup.js
+++ basic-extension/popup.js
@@ -1,8 +1,12 @@
 console.log("background running");
 document.getElementById('snip').addEventListener('click', onSnip, false)
 document.getElementById('csv').addEventListener('click', csv, false)
+document.getElementById('start').addEventListener('click', start, false)
 
 
+function clickme(){
+  alert("click")
+}
 
 function onSnip() {
   chrome.tabs.getSelected(null, function(tab){
@@ -12,18 +16,24 @@ function onSnip() {
 
       // send data to background script
       chrome.runtime.sendMessage(response.data);
-      
-      
-     
-      
-      
-      
-      
-  
     });
 });
 }
 
+function start(){
+  window.open('http://127.0.0.1:5000/start', '_blank')
+}
+
 function csv() {
   // download the csv file that has been building
+  window.open('http://127.0.0.1:5000/download', '_blank');
 }
+
+
+
+/* workflow
+
+- click pop up
+- press start
+- start takes you to new page... allows you to create a list of categories
+*/
