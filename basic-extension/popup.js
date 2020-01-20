211c908fc7a29c8b50237df3e6ed294b961fb48d
Jonathan  Foster <jonathanfoster@Jonathans-MacBook-Pro.local>
2020-01-20 19:38:08 +0000

    List items can be manipulated by user in the popup

--- basic-extension/popup.js
+++ basic-extension/popup.js
@@ -2,18 +2,35 @@ console.log("background running");
 document.getElementById('snip').addEventListener('click', onSnip, false)
 document.getElementById('csv').addEventListener('click', csv, false)
 document.getElementById('start').addEventListener('click', start, false)
+document.getElementById('add').addEventListener('click', add, false)
+document.getElementById('remove').addEventListener('click', remove, false)
 
+// list of columns
+var colList = document.getElementById("colList");
 
-function clickme(){
-  alert("click")
+// Add new column to the list
+function add() {
+  // Create a <li> node
+  var node = document.createElement("LI"); 
+  // Create a text node                
+  var textnode = document.createTextNode(document.getElementById("fname").value);  
+  // Append the text to <li>       
+  node.appendChild(textnode); 
+  // Append <li> to <ul> with id="colList"                             
+  colList.appendChild(node);     
+}
+
+function remove() {
+  var len = colList.childNodes.length;
+  console.log(len);
+  colList.removeChild(colList.childNodes[len - 1]);
 }
 
 function onSnip() {
   chrome.tabs.getSelected(null, function(tab){
     
     chrome.tabs.sendRequest(tab.id, {method: "getSelection"}, function(response){
-      
-
+    
       // send data to background script
       chrome.runtime.sendMessage(response.data);
     });
